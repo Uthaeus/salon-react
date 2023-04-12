371af77c03c328e44070ae548c25bfe694fd6199
Uthaeus <romanlavery@gmail.com>
2023-04-11 20:00:36 -0600

    added image for main background, installed popper

--- src/components/Navigation/main-navigation.js
+++ src/components/Navigation/main-navigation.js
@@ -7,7 +7,7 @@ function MainNavigation() {
             <div className=" nav-container">
 
                 <div className='container d-flex justify-content-evenly align-items-center'>
-                    <img src='../assets/asl_logo.jpg' alt='logo' />
+                    <img src='../../assets/images/asl_logo.jpg' alt='logo' width='20' height='20' />
                     <NavLink to='/' end className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>Home</NavLink>
                     <NavLink to='about' className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>About</NavLink>
                     <NavLink to='contact' className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>Contact</NavLink>
@@ -48,6 +48,16 @@ function MainNavigation() {
                 </div>
 
             <div className="d-flex align-items-center justify-content-end">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
                 <NavLink to='login' className={({isActive}) => isActive ? 'nav-link mx-2 active' : 'nav-link mx-2'}>Log In</NavLink>
                 <NavLink to='sign-up' className={({isActive}) => isActive ? 'nav-link mx-2 active' : 'nav-link mx-2'}>Sign Up</NavLink>
 
